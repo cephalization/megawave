8f23f3e5b643cfeee2496c69d399e75c9463a66b
Connor <dalek124@gmail.com>
2020-12-06 17:43:55 -0500

    Updated start-dev.bat to work properly on Windows.

--- start-dev.bat
+++ start-dev.bat
@@ -1,1 +1,1 @@
-npx concurrently -p "[{name}]" -n "API,WEB" -c "bgGreen.bold,bgBlue.bold" "cd packages\api; poetry run flask run" "cd packages\web; npm start"
+npx cross-env npx concurrently -p "[{name}]" -n "API,WEB" -c "bgGreen.bold,bgBlue.bold" "cd packages\api && poetry run flask run" "cd packages\web && npm start"
