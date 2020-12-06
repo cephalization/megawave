da9feb3ba33c30c6f0f7d0be05d3e0c3fc07a60c
Tony Powell <anthonypowell@hey.com>
2020-12-05 23:09:41 -0500

    web: Add sign in button to home page

--- packages/web/src/components/views/Home/Home.tsx
+++ packages/web/src/components/views/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PageContainer from '~/components/templates/PageContainer/PageContainer';
 
 export function Home() {
@@ -17,22 +18,32 @@ export function Home() {
 
   return (
     <PageContainer>
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <h2 className="inline text-3xl font-bold tracking-tight text-gray-900 sm:block sm:text-4xl">
-          Want music?
-        </h2>
-        <p className="inline text-3xl font-bold tracking-tight text-indigo-600 sm:block sm:text-4xl">
-          Stream with <b className="font-extrabold">Megawave</b>.
-        </p>
+      <div className="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 flex justify-between">
+        <div>
+          <h2 className="inline text-4xl font-bold tracking-tight text-gray-900 sm:block sm:text-4xl">
+            Want music?
+          </h2>
+          <p className="inline text-4xl font-bold tracking-tight text-indigo-600 sm:block sm:text-4xl">
+            Stream with <b className="font-extrabold">Megawave</b>.
+          </p>
+        </div>
+        <div className="justify-end self-end">
+          <Link
+            to="/login"
+            className="whitespace-nowrap hover:bg-gray-50 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex items-center justify-center text-base font-medium text-indigo-700 bg-indigo-100"
+          >
+            Sign in
+          </Link>
+        </div>
       </div>
 
-      <div className="bg-white sm:rounded-lg">
-        <div className="px-4 py-5 sm:p-6">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">
+      <div className="bg-white sm:rounded-lg max-w-6xl mx-auto mt-48">
+        <div className="px-4 py-6 sm:p-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-500">
             Debug Server Response
           </h3>
           <div className="mt-5">
-            <pre className="rounded-md bg-gray-200 px-6 py-6 sm:flex sm:items-start sm:justify-between text-gray-700">
+            <pre className="rounded-md bg-gray-50 px-6 py-6 sm:flex sm:items-start sm:justify-between text-gray-700">
               {data || 'Fetching...'}
             </pre>
           </div>
