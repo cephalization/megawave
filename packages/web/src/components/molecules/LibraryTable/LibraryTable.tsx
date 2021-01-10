7d8a9d81b4a86a2027b08d301b0acefaac118319
Tony Powell <anthonypowell@hey.com>
2021-01-10 16:27:49 -0500

    web: Virtualize LibraryTable

--- packages/web/src/components/molecules/LibraryTable/LibraryTable.tsx
+++ packages/web/src/components/molecules/LibraryTable/LibraryTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import { getLibrary, getLibraryResponse } from '~/queries/library';
+import { useVirtual } from 'react-virtual';
+import { getLibrary } from '~/queries/library';
 import { Track } from '~/types/library';
 import { WaveLoader } from '../WaveLoader';
 import {
@@ -13,22 +14,37 @@ export function LibraryTable() {
     'library',
     getLibrary,
   );
+  const parentRef = React.useRef(null);
+  const rowVirtualizer = useVirtual({
+    size: data?.length ?? 0,
+    parentRef,
+    estimateSize: React.useCallback(() => 45, [data]),
+    overscan: 10,
+  });
 
   if (isLoading || data === undefined) return <WaveLoader />;
 
   if (error) return null;
 
   return (
-    <div className="block">
+    <div className="block" ref={parentRef} style={{ overflow: 'auto' }}>
       <div className="align-middle inline-block min-w-full border-b border-gray-200">
         <table className="min-w-full">
           <thead>
             <LibraryTableHeaderRow />
           </thead>
-          <tbody className="bg-white divide-y divide-gray-100">
-            {data.map((track: Track) => (
-              <LibraryTableRow key={track.id} track={track} />
-            ))}
+          <tbody
+            style={{
+              height: `${rowVirtualizer.totalSize}px`,
+              width: '100%',
+              position: 'relative',
+            }}
+            className="bg-white divide-y divide-gray-100"
+          >
+            {rowVirtualizer.virtualItems.map(({ index }) => {
+              const track = data[index];
+              return <LibraryTableRow key={track.id} track={track} />;
+            })}
           </tbody>
         </table>
       </div>
