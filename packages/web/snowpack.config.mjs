1f3d520b94b05d2251492bdb7acfb0941e542abe
Tony Powell <anthonypowell@hey.com>
2021-05-23 10:09:37 -0400

    web: fix proxy crashing

--- packages/web/snowpack.config.mjs
+++ packages/web/snowpack.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 import proxy from 'http2-proxy';
+import throttle from 'lodash.throttle';
+
+const proxyConfig = {
+  hostname: 'localhost',
+  port: 5000,
+};
+
+const dProxyWeb = throttle(proxy.web, 3000);
+
+function handleError(err, req, res) {
+  if (err) console.warn('Server not ready... retrying in 3 seconds...');
+
+  if (res.headersSent) return;
+
+  return dProxyWeb(req, res, proxyConfig, handleError);
+}
 
 export default {
   mount: {
@@ -23,10 +39,7 @@ export default {
         // remove /api prefix (optional)
         req.url = req.url.replace(/^\/api/, '');
 
-        return proxy.web(req, res, {
-          hostname: 'localhost',
-          port: 5000,
-        });
+        return proxy.web(req, res, proxyConfig, handleError);
       },
     },
   ],
