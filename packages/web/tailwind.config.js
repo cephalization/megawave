61d0fd22f79dac7a813d912b6ac29c933ae98165
Anthony Powell <anthonypowell@hey.com>
2021-07-31 21:27:58 -0400

    web: upgrade tailwind deps

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -14,5 +14,9 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
