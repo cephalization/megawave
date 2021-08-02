de2e777879c0692da0fdf0c1a0f1e9c21d2404e3
Anthony Powell <anthonypowell@hey.com>
2021-08-02 15:29:30 -0400

    api: implement sorting for /songs

--- packages/api/megawave/audio.py
+++ packages/api/megawave/audio.py
@@ -1,5 +1,6 @@
 import os
 from typing import Dict, List, Tuple, Union
+from typing_extensions import TypedDict
 from mutagen import MutagenError
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
@@ -10,8 +11,22 @@ from megawave.id import getId
 
 VALID_AUDIO_EXTENSIONS = ["wav", "mp3"]
 
+AudioFile_Serialized = TypedDict(
+    "AudioFile_Serialized",
+    {
+        "name": str,
+        "album": Union[List[str], None],
+        "artist": Union[List[str], None],
+        "length": Union[int, None],
+        "id": str,
+        "link": str,
+        "meta": str,
+        "fileType": str,
+    },
+)
 
-def hasAudioFileExtension(fileName: str) -> Union[Tuple[bool, str], bool]:
+
+def hasAudioFileExtension(fileName: str) -> Tuple[bool, Union[str, None]]:
     fileExt = fileName.split(".")[-1]
     if fileExt in VALID_AUDIO_EXTENSIONS:
         return True, fileExt
@@ -26,13 +41,13 @@ class AudioFile:
     Contains information about a discovered audio file.
     """
 
-    def __init__(self, rootDir: str, fileName: str, fileType: str):
+    def __init__(self, rootDir: str, fileName: str, fileType: Union[str, None]):
         self.ok = True
         self.rootDir: str = rootDir
         self.fileName: str = fileName
         self.filePath: str = os.path.join(rootDir, fileName)
         self.fileDir: str = os.path.abspath(rootDir)
-        self.fileType: str = fileType
+        self.fileType: str = fileType or ""
         self.id: str = getId()
         self.meta = None
         self.info = None
@@ -52,7 +67,7 @@ class AudioFile:
         except ValueError:
             self.ok = False
 
-    def serialize(self):
+    def serialize(self) -> Union[AudioFile_Serialized, None]:
         """Convert AudioFile into a representation that can be sent over the
         wire as JSON
         """
@@ -63,12 +78,21 @@ class AudioFile:
             "name": self.meta.get("title", [self.fileName])[0],
             "album": self.meta.get("album", None),
             "artist": self.meta.get("artist", None),
-            "length": self.info.length,
+            "length": self.info.length if self.info is not None else None,
             "id": self.id,
             "link": f"/api/songs/{self.id}",
             "meta": self.meta.pprint(),
             "fileType": self.fileType,
         }
+
+    @staticmethod
+    def getSafeArtist(audio: AudioFile_Serialized) -> str:
+        artist_value = audio.get("artist")
+
+        if artist_value is not None:
+            return ", ".join(artist_value) or ""
+
+        return ""
 
 
 class AudioLibrary:
@@ -103,13 +127,15 @@ class AudioLibrary:
 
         return output
 
-    def serialize(self) -> List[Dict[str, str]]:
+    def serialize(self) -> List[AudioFile_Serialized]:
         """Convert AudioLibrary into a representation that can be sent over
         the wire as JSON
         """
-        output: List[Dict[str, str]] = []
+        output: List[AudioFile_Serialized] = []
         for entry in self.entries():
             if entry is not None:
-                output.append(entry.serialize())
+                serialized = entry.serialize()
+                if serialized is not None:
+                    output.append(serialized)
 
         return output
