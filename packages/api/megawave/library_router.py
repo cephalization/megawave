52856871f39562534bcf1021ba0d8d8869d23028
Anthony Powell <anthonypowell@hey.com>
2021-08-02 23:51:42 -0400

    api: fix streaming audio partial content

--- packages/api/megawave/library_router.py
+++ packages/api/megawave/library_router.py
@@ -1,9 +1,26 @@
+import os
 from typing import Optional, Union
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 from megawave import files
 from megawave.audio import AudioFile, AudioFile_Serialized
 from starlette.responses import FileResponse, StreamingResponse
+
+BYTES_PER_RESPONSE = 325160
+
+# https://github.com/tiangolo/fastapi/issues/1240
+# todo: move this
+def chunk_generator_from_stream(stream, chunk_size, start, size):
+    bytes_read = 0
+
+    stream.seek(start)
+
+    while bytes_read < size:
+        bytes_to_read = min(chunk_size, size - bytes_read)
+        yield stream.read(bytes_to_read)
+        bytes_read = bytes_read + bytes_to_read
+
+    stream.close()
 
 
 def get_audio_file_sort_value(
@@ -47,17 +64,37 @@ def songs(sort: Optional[str] = None):
 
 
 @router.get("/songs/{id}")
-def song(id: str):
+def song(id: str, req: Request):
     song = files.audioLibrary.getById(id)
-    if song is not None:
 
-        def iterfile():
-            with open(song.filePath, mode="rb") as file_like:
-                yield from file_like
+    # todo: move this streaming logic
+    if song is not None:
+        asked = req.headers.get("Range")
+
+        # todo: optimize this, songs take too long to start streaming
+        stream = open(song.filePath, mode="rb")
+        total_size = os.path.getsize(song.filePath)
+
+        start_byte_requested = int(asked.split("=")[-1][:-1])
+        end_byte_planned = (
+            min(start_byte_requested + BYTES_PER_RESPONSE, total_size) - 1
+        )
+
+        chunk_generator = chunk_generator_from_stream(
+            stream,
+            chunk_size=50000,
+            start=start_byte_requested,
+            size=BYTES_PER_RESPONSE,
+        )
 
         return StreamingResponse(
-            iterfile(),
-            media_type=get_media_type(song.serialize()),
+            chunk_generator,
+            headers={
+                "Accept-Ranges": "bytes",
+                "Content-Range": f"bytes {start_byte_requested}-{end_byte_planned}/{total_size}",
+                "Content-Type": get_media_type(song.serialize()),
+            },
+            status_code=206,
         )
 
     return {"error": {"message": "not found"}}, 404
