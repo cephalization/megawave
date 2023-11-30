26230d4449de84d620c2c6ae224af63369d42884
Anthony Powell <powell.anthonyd@proton.me>
2023-11-30 15:49:16 -0500

    Modernize vite, tailwind, and python configs

--- packages/web/tailwind.config.ts
+++ packages/web/tailwind.config.ts
@@ -1,18 +1,14 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 module.exports = {
-  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
-  mode: 'jit',
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class',
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
