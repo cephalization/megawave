fe4d3cd5302a8c9f367ad7045be857fc1ecc90c1
Anthony Powell <powell.anthonyd@proton.me>
2023-11-30 17:59:41 -0500

    Cleanup resposivity of control bar

--- packages/web/src/components/atoms/CurrentTrack/CurrentTrack.tsx
+++ packages/web/src/components/atoms/CurrentTrack/CurrentTrack.tsx
@@ -5,27 +5,38 @@ import { getArrayString } from '~/utils/trackMeta';
 type CurrentTrackProps = {
   title?: string;
   artist?: string | string[] | null;
+  art?: string;
 };
 
-export const CurrentTrack = ({ title, artist }: CurrentTrackProps) => {
+export const CurrentTrack = ({ title, artist, art }: CurrentTrackProps) => {
   if (!title) return null;
 
   const formattedArtist = getArrayString(artist);
 
   return (
-    <div className="flex flex-wrap text-sm px-5 sm:px-6 lg:px-8 py-4 max-w-full overflow-hidden">
-      <label
-        title={title}
-        className="font-bold overflow-ellipsis w-full max-w-full whitespace-nowrap"
-      >
-        {title}
-      </label>
-      <label
-        title={formattedArtist}
-        className="text-gray-600 hover:text-gray-800 overflow-ellipsis w-full max-w-full whitespace-nowrap overflow-hidden"
-      >
-        {formattedArtist}
-      </label>
+    <div className='flex gap-2 items-center max-w-full'>
+      <div className="flex-shrink-0 md:pl-0">
+        <img
+          className="object-contain rounded"
+          style={{ height: 32, width: 32 }}
+          src={art}
+          alt={`Album art for ${title} by ${artist}`}
+        />
+      </div>
+      <div className="grid grid-rows-2 text-sm max-w-full">
+        <label
+          title={title}
+          className="font-bold truncate w-full max-w-full whitespace-nowrap"
+        >
+          {title}
+        </label>
+        <label
+          title={formattedArtist}
+          className="text-gray-600 hover:text-gray-800 truncate w-full max-w-full whitespace-nowrap"
+        >
+          {formattedArtist}
+        </label>
+      </div>
     </div>
   );
 };
--- packages/web/src/components/molecules/Controls/Controls.tsx
+++ packages/web/src/components/molecules/Controls/Controls.tsx
@@ -33,50 +33,55 @@ export function Controls() {
 
   return (
     <>
-      <div className="flex w-full justify-start bg-gray-100 dark:bg-gray-900 h-20  border-t border-gray-200 relative bottom-0 z-50 py-1">
-        <div className="flex w-1/4">
-          <CurrentTrack title={track?.name} artist={track?.artist} />
-        </div>
-        <div className="flex w-2/4 flex-wrap">
-          <audio
-            ref={audioRef}
-            title={`${track?.name} ${
-              track?.artist ? `- ${track?.artist}` : ''
-            }`}
-          ></audio>
-          <div className="text-gray-600 dark:text-white lg:rounded-b-xl px-1 sm:px-3 lg:px-1 xl:px-3 flex w-full justify-center items-center">
-            <PrevButton onClick={handlePrev} />
-            <PlayPauseButton
-              playing={playing}
-              onPause={handlePause}
-              onPlay={handlePlay}
+      <div className="grid grid-rows-2 w-full bg-gray-100 dark:bg-gray-900 h-20  border-t border-gray-200 relative bottom-0 z-50 py-1">
+        <div className="flex basis-3/4 w-full justify-between">
+          <div className="flex w-full justify-start pl-4 pr-1">
+            <CurrentTrack
+              title={track?.name}
+              art={track?.art?.[0]}
+              artist={track?.artist}
             />
-            <NextButton onClick={handleNext} />
           </div>
-
-          <div className="flex w-full items-center px-12">
-            <div className="text-sm mr-1 tabular-nums">
+          <div className="flex w-full flex-shrink">
+            <audio
+              ref={audioRef}
+              title={`${track?.name} ${
+                track?.artist ? `- ${track?.artist}` : ''
+              }`}
+            ></audio>
+            <div className="text-gray-600 dark:text-white lg:rounded-b-xl px-1 sm:px-3 lg:px-1 xl:px-3 flex w-full justify-center items-center">
+              <PrevButton onClick={handlePrev} />
+              <PlayPauseButton
+                playing={playing}
+                onPause={handlePause}
+                onPlay={handlePlay}
+              />
+              <NextButton onClick={handleNext} />
+            </div>
+          </div>
+          <div className="flex flex-shrink w-full justify-center items-center">
+            <button
+              className="sm:block mx-1 hover:text-gray-900"
+              onClick={() => setOpen((o) => !o)}
+            >
+              <Bars3Icon height={24} />
+            </button>
+          </div>
+        </div>
+        <PlayHistory open={open} setOpen={setOpen} />
+        <div className="flex w-full basis-1/4 items-center justify-center px-4">
+          <div className="flex w-full items-center justify-center max-w-2xl gap-1">
+            <div className="text-sm tabular-nums">
               {formatTime(currentTime)}
             </div>
             <ProgressBar
               onChange={handleScrub}
               percentage={durationPercentage}
             />
-            <div className="text-sm ml-1 tabular-nums">
-              {formatTime(duration)}
-            </div>
+            <div className="text-sm tabular-nums">{formatTime(duration)}</div>
           </div>
         </div>
-        <div className="flex w-1/4">
-          <button
-            className="sm:block mx-1 hover:text-gray-900"
-            onClick={() => setOpen((o) => !o)}
-          >
-            <Bars3Icon height={24} />
-          </button>
-        </div>
       </div>
-      <PlayHistory open={open} setOpen={setOpen} />
     </>
   );
 }
