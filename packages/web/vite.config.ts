df1a753aa15ece54327e188777950558484f6779
Anthony Powell <powell.anthonyd@gmail.com>
2025-02-02 19:18:47 -0500

    Prep vite for tailwind plugin

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,10 +1,14 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ babel: { plugins: ['babel-plugin-react-compiler'] } })],
+  plugins: [
+    react({ babel: { plugins: ['babel-plugin-react-compiler'] } }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, '/src'),
