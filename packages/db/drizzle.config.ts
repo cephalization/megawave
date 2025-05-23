ab25a55edaeb92a632a3e438eea5916396e02bdb
Anthony Powell <powell.anthonyd@gmail.com>
2025-05-23 14:06:27 -0400

    Safely concatenate database path in migrator

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   schema: "./packages/db/src/schema.ts",
   out: "./packages/db/drizzle",
   dbCredentials: {
-    url: `file:${process.env.DATABASE_PATH!}/megawave.db`,
+    url: `file:${
+      process.env.DATABASE_PATH!.endsWith("/")
+        ? process.env.DATABASE_PATH!
+        : `${process.env.DATABASE_PATH!}/`
+    }megawave.db`,
   },
 });
